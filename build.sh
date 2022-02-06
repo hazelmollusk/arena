88d450e3204a99cf41cfc80b760e691fc689d6c1
Matt Barry <matt@hazelmollusk.org>
2022-02-06 10:04:50 -0500

    build

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+pushd `dirname $0`
+rm -rf env node_modules
 python3 -mvenv env
 source ./env/bin/activate
 pip3 install -r requirements.txt
 npm install
 npm run build
 ./manage.py collectstatic --noinput
+popd
